01175fffc1fbf5da4ae4056d6ae1b557dbb19cfd
Rishabh-Kumar01 <rishabh1teotia@gmail.com>
2024-10-05 11:46:51 +0530

    chore: Remove redundant token check in auth.middleware.js

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -63,10 +63,6 @@ export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    return next(new CustomError("Authentication token is required", 401));
-  }
-
   jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, user) => {
     if (err) {
       return next(new CustomError("Invalid or expired token", 403));
